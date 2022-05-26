bfd63fd6280c501b020eb3583f2964f3bb7476c2
florizdesigner <vyacheslav.sokolovv@icloud.com>
2022-05-26 14:01:16 +0300

    build on vitejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+    build: {
+        watch: {
+            // https://rollupjs.org/guide/en/#watch-options
+        },
+        base: "./"
+
+    }
 })
